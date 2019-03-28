cdf3c2bc8a79cdf687892d287ea790cae40f8017
Benjamin Van Iseghem <benjamin.vaniseghem.w3891@student.hogent.be>
2019-03-28 15:05:25 +0100

    added object log functionality

--- log/log.go
+++ log/log.go
@@ -5,7 +5,10 @@ import (
 	"github.com/opentracing/opentracing-go/log"
 )
 
-//Error adds error logs to the spans and returns it
+//Future functionality will include DEBUG and WARNING messages which can be filtered or excluded in jaeger
+//LOGRUS support will be implemented if necessary (to eliminate duplicate code)
+
+//Error adds error logs to the span and returns it
 func Error(span opentracing.Span, err error, s string) opentracing.Span {
 	span.LogFields(
 		log.String("Error message", s),
@@ -14,20 +17,35 @@ func Error(span opentracing.Span, err error, s string) opentracing.Span {
 	return span
 }
 
-//StatusCode adds statuscode logs to the spans and returns it
-func StatusCode(i int) {
-
-}
-
-//String adds string logs to the spans and returns it
-func String(span opentracing.Span, s string) opentracing.Span {
+//StatusCode adds statuscode logs to the span and returns it
+func StatusCode(span opentracing.Span, i int) opentracing.Span {
 	span.LogFields(
-		log.String("Message", s),
+		log.Int("Statuscode", i),
 	)
 	return span
 }
 
-//Int adds integer logs to the spans and returns it
-func Int(i int) {
+//String adds string logs to the span and returns it
+func String(span opentracing.Span, s string) opentracing.Span {
+	span.LogFields(
+		log.String("Info", s),
+	)
+	return span
+}
 
+//Int adds integer logs to the span and returns it
+func Int(span opentracing.Span, s string, i int) opentracing.Span {
+	span.LogFields(
+		log.Int(s, i),
+	)
+	return span
+}
+
+//Object adds an object log to the span and returns it
+func Object(span opentracing.Span, s string, obj interface{}) opentracing.Span {
+	span.LogFields(
+		log.Object(s, obj),
+	)
+
+	return span
 }
